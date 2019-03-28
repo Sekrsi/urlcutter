462f194dec2bf40e73d453b8913220e8a54ea577
Rafal <rafal.patro@gmail.com>
2019-03-28 14:12:15 +0100

    v1

---
+++ src/main/java/com/urlshortener/ox/Controllers/UserController.java
@@ -0,0 +1,10 @@
+package com.urlshortener.ox.Controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping({"/user","/user/"})
+public class UserController {
+}
